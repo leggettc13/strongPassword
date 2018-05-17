1f61601b33a8b31e5f683e9146789526766e2b9e
Christopher Leggett <leggettc13@protonmail.com>
2018-05-16 23:45:36 -0400

    Initial Commit

---
+++ strongPassword.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+# strongPassword - A program that uses regex to determine password strength
+
+# TODO: Define a regex for a weak password
+
+# TODO: Define a regex for an average password
+
+# TODO: Define a regex for a strong password
+
+# TODO: Get the password from user input
+
+# TODO: Match the password with the different regex to determine strength
